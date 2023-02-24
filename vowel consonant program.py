58537953255f82045c5e564035fffcb32b6d9ffa
aman12z <119755989+aman12z@users.noreply.github.com>
2023-02-24 21:28:08 +0530

    Add files via upload

---
+++ vowel consonant program.py
@@ -0,0 +1,7 @@
+char=input("enter the alphabet ")
+if(char in 'aeiou'):
+    print("character is vowel")
+elif(char in 'AEIOU'):
+    print("character is vowel")
+else:
+    print("character is consonanat or invalid input")
